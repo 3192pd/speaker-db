3c96e4aa4ad96c7c4f1a463ca3ef87d053e395d8
Stieilijh <saitushar02@gmail.com>
2024-05-02 14:55:18 +0530

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  optimizeDeps: {
-    esbuildOptions: {
-      target: "esnext",
-    },
-  },
   build: {
-    target: "esnext",
+    target: "esnext", //browsers can handle the latest ES features
   },
 })
